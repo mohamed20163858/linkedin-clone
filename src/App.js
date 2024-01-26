9501f4c0e3fd8a230d8be48740357c9f8f7a40bf
mohamed saleh <mohamed20163858@gmail.com>
2024-01-26 18:45:37 +0200

    before material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>let's build a linkedin clone</h1>
       {/* Header */}
+      <Header />
       {/* App body */}
         {/* sidebar */}
         {/* feed */}
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header">
+        {/* header_left */}
+        <div className="header_left">
+            <img src="" alt="" />
+            <div className="header_search">
+                
+                <input type="text" name="" id="" />
+            </div>
+        </div>
+        
+        {/* header_right */}
+    </div>
+  )
+}
+
+export default Header
